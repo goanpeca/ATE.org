a07f02e18f009d5a0ebc894bc4c1c2b0c7b29f37
Tom Hören <tom.hoeren@micronas.com>
2020-05-08 20:07:11 +0200

    .

---
+++ src/ATE/org/coding/FT_common_generator.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  8 20:06:39 2020
+
+@author: hoeren
+"""
+
---
+++ src/ATE/org/coding/HW_common_generator.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  8 20:06:26 2020
+
+@author: hoeren
+"""
+
